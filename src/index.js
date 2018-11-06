4ba9b8c48c927c7869e774c1b5c09adb4f4619ca
github username <github email address>
2018-11-06 16:21:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from './Login'
+import About from './About'
+import Navbar from './Navbar'
 
 const Home = () => {
   return (
     <div>
       <h1>Home!</h1>
     </div>
-  );
-};
+  )
+}
+
+
+
+
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <React.Fragment>
+      <Navbar/>
+      <Route path="/" render={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/about" component={About} />
+    </React.Fragment>
+  </Router>,
   document.getElementById('root')
 );
